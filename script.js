dba0adb09ecf63e09bb000df07b246727d0e8820
jitendra19 <jiten.cs2010@gmail.com>
2020-09-19 23:08:48 +0530

    promise calls

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello!')
+function delay(ms) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(()=>{
+      console.log('asdf')
+      resolve();
+    }, ms);
+  });
+  // your code
+}
+
+delay(1000).then(() => alert('runs after 1 seconds'));
