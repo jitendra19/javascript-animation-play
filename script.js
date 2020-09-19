7c18a5ceef009566fd3bf0bc7e4cffc916c4d3b7
jitendra19 <jiten.cs2010@gmail.com>
2020-09-19 23:14:29 +0530

    new form of setTimeout

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function delay(ms) {
   return new Promise(function(resolve, reject) {
-    setTimeout(()=>{
-      console.log('asdf')
-      resolve();
-    }, ms);
-  });
-  // your code
+    setTimeout(()=>resolve(), ms);
+  });  
 }
 
-delay(1000).then(() => alert('runs after 1 seconds'));
+delay(1000).then(() => alert('runs after delay 1 seconds'));
+
+
+// another form of settimeout
+function delay1(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));  
+}
+delay1(1000).then(() => alert('runs after same 1 seconds'));
